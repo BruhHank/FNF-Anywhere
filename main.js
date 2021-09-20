e8bbc932300c2159e977c5993f4680aeaf8269a7
Coal <44954117+Cole320@users.noreply.github.com>
2021-09-20 12:20:09 -0400

    Add semicolons to fix UB

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 alert("Prepare For Gaming <3");
 html = document.querySelector("html");
-document.removeChild(html)
+document.removeChild(html);
 
 newHTML = document.createElement("html");
 newHTML.innerHTML = `
 insert_html_here
   
-`
+`;
 
 document.appendChild(newHTML);
 body = document.querySelector("body");
